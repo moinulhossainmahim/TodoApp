de0be10f761b8fa03031cf944abc8aaff378e667
Moinul Hossain <moinulhossainmahim@gmail.com>
2021-01-31 19:51:49 +0600

    Added input validation and priority badge

--- js/script.js
+++ js/script.js
@@ -1,45 +1,68 @@
 (function() {
-
-	const taskNumber 	  = document.getElementById('task-name');
-	const taskTitle 	  = document.getElementById('task-title');
-	const taskDescription = document.getElementById('description-box');
-	const taskPriority    = document.getElementById('dropdown-box');
-	const addTaskButton   = document.getElementById('add-btn');
-	const taskShowContainer  = document.querySelector('.task-show-container');
-	const itemDiv 	      = document.getElementById('item-div');
-
-	var taskNameValue 		 = '';
-	var taskTitleValue 		 = '';
-	var taskDescriptionValue = '';
-	var taskPriorityValue 	 = '';
-	var tasksList 			 = [];
+	const taskTitle 	  	 	 = document.getElementById('task-title');
+	const taskDescription 	 	 = document.getElementById('description-box');
+	const taskPriority    	 	 = document.getElementById('dropdown-box');
+	const addTaskButton   	 	 = document.getElementById('add-btn');
+	const taskShowContainer  	 = document.querySelector('.task-show-container');
+	const itemDiv 	      	  	 = document.getElementById('item-div');
+	const titleAlertMessage 	 = document.getElementById('title-alert-msg');
+	const descriptionAlertMessage= document.getElementById('description-alert-msg');
+	const priorityAlertMessage 	 = document.getElementById('priority-alert-msg');
+	var tasksList 			 	 = [];
 
 	addTaskButton.addEventListener('click', function() {
 
-		var itemsHtml = '<h1>Task list</h1>';
+		var itemsHtml = '<h1>Task List</h1>';
 
-
-		taskNameValue 		 = '';
-		taskTitleValue 		 = '';
-		taskDescriptionValue = '';
-		taskPriorityValue 	 = '';
-
-		var titleValue = taskTitle.value;
+		var titleValue 	= taskTitle.value;
 		var descriptionValue = taskDescription.value;
 		var priorityValue = taskPriority.value;
 
+		if (titleValue ==='') {
+			titleAlertMessage.style.display = 'block';
+		} else {
+			titleValue;
+			titleAlertMessage.style.display = 'none';
+		}
+		if (descriptionValue ==='') {
+			descriptionAlertMessage.style.display = 'block';
+		} else {
+			descriptionValue;
+			descriptionAlertMessage.style.display = 'none';
+		}
+		if (priorityValue ==='') {
+			priorityAlertMessage.style.display = 'block';
+		} else {
+			priorityValue;
+			priorityAlertMessage.style.display = 'none';
+		}
+
+		if((titleValue ==='') || (descriptionValue ==='') || (priorityValue ==='')) {
+			taskShowContainer.style.display = 'none';
+
+		} else {
+			taskShowContainer.style.display = 'block';
+		}
+
 		tasksList.push({title:titleValue, description:descriptionValue, priority:priorityValue});
-		console.log(tasksList);
 		tasksList.forEach(function(objectInArray, index){
+			var badgeElements ='';
+			if(objectInArray.priority == 1) {
+				badgeElements = '<span class="badge-high">High</span>';
+			} else if(objectInArray.priority == 2) {
+				badgeElements = '<span class="badge-medium">Medium</span>';
+			} else if(objectInArray.priority == 3) {
+				badgeElements = '<span class="badge-low">Low</span>';
+			}
+
 			itemsHtml += `<div id="item-div">
 					<p id="task-name"><strong>Task:</strong> ${index+1}</p>
 					<p id="task-title"><strong>Title:</strong> ${objectInArray.title}</p>
 					<p id="task-description"><strong>Description:</strong> ${objectInArray.description}</p>
-					<p id="task-priority"><strong>Priority:</strong> ${objectInArray.priority}</p>
+					<p id="task-priority"><strong>Priority:</strong> ${badgeElements}</p>
 				</div>`;
 		});
 		taskShowContainer.innerHTML = itemsHtml;
 	});
 			
-
 })();
